d9647049977e72be15c8a4dd19b7ae8531d5f497
White Demon <133562822+whitedemon938@users.noreply.github.com>
2025-03-03 17:27:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-source mkvenv/bin/activate 
-
-gunicorn -b :$PORT app:app & 
-python3 update.py &          
-python3 -m bot
+gunicorn -b :$PORT app:app && python3 update.py & python3 -m bot
